07ee04b7b4318f3bcca335f27602a2ed59b99b30
Alex Koziell <alexkoziell@gmail.com>
2020-06-14 14:25:50 +0100

    Easier fractal controls.

--- scripts/fractals.js
+++ scripts/fractals.js
@@ -1,10 +1,12 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-const slider = document.getElementById('slider');
+const slider = document.getElementById('base-slider');
 const baseDisplay = document.getElementById('base-value');
 
-const MAX_DEPTH = 5;
+const MAX_DEPTH = 6;
+const LEVEL_HEIGHT = 10;
+const LEVEL_SEP = 15;
 
 function drawFractal(x0, y0, w, h, depth, n) {
   context.fillStyle = 'hsl(' + 360 * Math.random() + ', 75%, 50%)';
@@ -12,13 +14,13 @@ function drawFractal(x0, y0, w, h, depth, n) {
   if (depth < MAX_DEPTH) {
     ++depth;
     for (var i = (-1/2)*(n%2-1); i < n; i += 2) {
-      drawFractal(x0 + i*w/n, y0+10, w/n, h, depth, n);
+      drawFractal(x0 + i*w/n, y0+LEVEL_SEP, w/n, h, depth, n);
     }
   }
 }
 
 baseDisplay.value = slider.value;
-drawFractal(0, 0, canvas.width, 5, 0, slider.value);
+drawFractal(0, 0, canvas.width, LEVEL_HEIGHT, 0, slider.value);
 
 slider.addEventListener('input', () => {
   baseDisplay.value = slider.value;
@@ -26,5 +28,5 @@ slider.addEventListener('input', () => {
 
 slider.addEventListener('change', () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  drawFractal(0, 0, canvas.width, 5, 0, slider.value);
+  drawFractal(0, 0, canvas.width, LEVEL_HEIGHT, 0, slider.value);
 });
